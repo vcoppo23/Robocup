5bcdc35f3c82a915a623f6a2feecd7a670713805
gmankowski23 <gmankowski23@bsmschool.org>
2022-11-07 12:50:21 -0600

    test

--- app.py
+++ app.py
@@ -3,41 +3,28 @@ from flask import Flask, render_template, Response, request
 from time import sleep
 import RPi.GPIO as GPIO
 from static import stepper
+GPIO.setmode(GPIO.BCM)			# GPIO numbering
+GPIO.setwarnings(False)			# enable warning from GPIO
+AN2 = 23 				# set pwm2 pin on MD10-Hat
+AN1 = 22				# set pwm1 pin on MD10-hat
+DIG2 = 18 				# set dir2 pin on MD10-Hat
+DIG1 = 17  				# set dir1 pin on MD10-Hat
+GPIO.setup(AN2, GPIO.OUT)		# set pin as output
+GPIO.setup(AN1, GPIO.OUT)		# set pin as output
+GPIO.setup(DIG2, GPIO.OUT)		# set pin as output
+GPIO.setup(DIG1, GPIO.OUT)		# set pin as output
+sleep(1)				   # delay for 1 seconds
+p1 = GPIO.PWM(AN1, 100)			# set pwm for M1
+p2 = GPIO.PWM(AN2, 100)	
 
 app = Flask(__name__) 
 
 @app.route('/')
 @app.route('/home', methods=['GET', 'POST'])
 def index(): 
-   if request.method == 'POST':
-      GPIO.setmode(GPIO.BCM)			# GPIO numbering
-      GPIO.setwarnings(False)			# enable warning from GPIO
-      AN2 = 23 				# set pwm2 pin on MD10-Hat
-      AN1 = 22				# set pwm1 pin on MD10-hat
-      DIG2 = 18 				# set dir2 pin on MD10-Hat
-      DIG1 = 17  				# set dir1 pin on MD10-Hat
-      GPIO.setup(AN2, GPIO.OUT)		# set pin as output
-      GPIO.setup(AN1, GPIO.OUT)		# set pin as output
-      GPIO.setup(DIG2, GPIO.OUT)		# set pin as output
-      GPIO.setup(DIG1, GPIO.OUT)		# set pin as output
-      sleep(1)				   # delay for 1 seconds
-      p1 = GPIO.PWM(AN1, 100)			# set pwm for M1
-      p2 = GPIO.PWM(AN2, 100)	   
+   if request.method == 'POST':    
       return render_template('home.html')
    else:
-      GPIO.setmode(GPIO.BCM)			# GPIO numbering
-      GPIO.setwarnings(False)			# enable warning from GPIO
-      AN2 = 23 				# set pwm2 pin on MD10-Hat
-      AN1 = 22				# set pwm1 pin on MD10-hat
-      DIG2 = 18 				# set dir2 pin on MD10-Hat
-      DIG1 = 17  				# set dir1 pin on MD10-Hat
-      GPIO.setup(AN2, GPIO.OUT)		# set pin as output
-      GPIO.setup(AN1, GPIO.OUT)		# set pin as output
-      GPIO.setup(DIG2, GPIO.OUT)		# set pin as output
-      GPIO.setup(DIG1, GPIO.OUT)		# set pin as output
-      sleep(1)				   # delay for 1 seconds
-      p1 = GPIO.PWM(AN1, 100)			# set pwm for M1
-      p2 = GPIO.PWM(AN2, 100)	 
       return render_template('home.html')
 
 @app.route('/forward', methods=['GET', 'POST'])
