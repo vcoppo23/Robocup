7c3e1e8f223a83e9c8bac455bb529d2956c17a0d
gmankowski23 <gmankowski23@bsmschool.org>
2022-11-16 11:33:35 -0600

    tank controls update

--- app.py
+++ app.py
@@ -1,22 +1,25 @@
 from re import U
 from flask import Flask, render_template, Response, request
 from time import sleep
-import RPi.GPIO as GPIO
 from static import stepper
-GPIO.setmode(GPIO.BCM)			# GPIO numbering
-GPIO.setwarnings(False)			# enable warning from GPIO
-AN2 = 23 				# set pwm2 pin on MD10-Hat
-AN1 = 22				# set pwm1 pin on MD10-hat
-DIG2 = 18 				# set dir2 pin on MD10-Hat
-DIG1 = 17  				# set dir1 pin on MD10-Hat
-GPIO.setup(AN2, GPIO.OUT)		# set pin as output
-GPIO.setup(AN1, GPIO.OUT)		# set pin as output
-GPIO.setup(DIG2, GPIO.OUT)		# set pin as output
-GPIO.setup(DIG1, GPIO.OUT)		# set pin as output
-sleep(1)				   # delay for 1 seconds
-p1 = GPIO.PWM(AN1, 100)			# set pwm for M1
-p2 = GPIO.PWM(AN2, 100)
+"""
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+AN2 = 23
+AN1 = 22
+DIG2 = 18
+DIG1 = 17
+GPIO.setup(AN2, GPIO.OUT)
+GPIO.setup(AN1, GPIO.OUT)
+GPIO.setup(DIG2, GPIO.OUT)
+GPIO.setup(DIG1, GPIO.OUT)
+p1 = GPIO.PWM(AN1, 100)
+p2 = GPIO.PWM(AN2, 100
+"""
 
+global current_speed
+current_speed = 0
 
 app = Flask(__name__) 
 
@@ -24,110 +27,32 @@ app = Flask(__name__)
 @app.route('/home', methods=['GET', 'POST'])
 def index(): 
    if request.method == 'POST':    
-      return render_template('home.html')
+      return render_template('gamepad.html')
    else:
-      return render_template('home.html')
-
-@app.route('/revdown', methods=['GET', 'POST'])
-def revdown():
-   speedlist=[]
-   speedlist = stepper.stepper(50,0)
-   GPIO.output(DIG1, GPIO.LOW)
-   GPIO.output(DIG2, GPIO.LOW)
-   for i in range(len(speedlist)):
-      p1.start(speedlist[i])
-      p2.start(speedlist[i])
-      print("Speed is at ",speedlist[i],"%")
-      sleep(0.25)
-   
-   return render_template('home.html')
+      return render_template('gamepad.html')
 
 @app.route('/forward', methods=['GET', 'POST'])
 def forward():
-   speedlist=[]
-   speedlist = stepper.stepper(0,50)
-   GPIO.output(DIG1, GPIO.LOW)
-   GPIO.output(DIG2, GPIO.LOW)
-   for i in range(len(speedlist)):
-      p1.start(speedlist[i])
-      p2.start(speedlist[i])
-      print("Speed is at ",speedlist[i],"%")
-      sleep(0.25)
-   sleep(1)
-   return render_template('home.html')
+   if request.method == 'POST':
 
-@app.route('/left', methods=['GET', 'POST'])
-def left():
-   i = 10
-   GPIO.output(DIG1, GPIO.LOW)
-   GPIO.output(DIG2, GPIO.HIGH)
-   while i<100:
-      p1.start(i)
-      p2.start(i)
-      print("Speed is at ",i, "%")
-      i+=10
-      sleep(0.25)
-   if i==100:
-      print("Holding for 3 seconds")
-      sleep(3)
-      while 0<=i<=100:
-         p1.start(i)
-         p2.start(i)
-         print("Speed is at ",i,"%")
-         i-=10
-         sleep(0.25)
-   else:
-      print("It's Val's fault")
-   return render_template('home.html')
+      joystick1 = request.form['joystick1']
+      joystick2 = request.form['joystick2']
+      joystick1 = int(float(joystick1)*100)
+      joystick2 = int(float(joystick2)*100)
+      print("Left % power",joystick1)
+      print("Right % power",joystick2)
+      if joystick1 < 0:
+         joystick1 = -joystick1
+         p1.start(joystick1)
+      else:
+         p1.start(joystick1)
+      if joystick2 < 0:
+         joystick2 = -joystick2
+         p2.start(joystick2)
+      else:
+         p2.start(joystick2)
 
-@app.route('/backward', methods=['GET', 'POST'])
-def backward():
-   i = 10
-   GPIO.output(DIG1, GPIO.HIGH)
-   GPIO.output(DIG2, GPIO.HIGH)
-   while i<100:
-      p1.start(i)
-      p2.start(i)
-      print("Speed is at ",i, "%")
-      i+=10
-      sleep(0.25)
-   if i==100:
-      print("Holding for 3 seconds")
-      sleep(3)
-      while 0<=i<=100:
-         p1.start(i)
-         p2.start(i)
-         print("Speed is at ",i,"%")
-         i-=10
-         sleep(0.25)
-   else:
-      print("It's Val's fault")
-   return render_template('home.html')
-
-@app.route('/right', methods=['GET', 'POST'])
-def right():
-   i = 10
-   GPIO.output(DIG1, GPIO.HIGH)
-   GPIO.output(DIG2, GPIO.LOW)
-   while i<100:
-      p1.start(i)
-      p2.start(i)
-      print("Speed is at ",i, "%")
-      i+=10
-      sleep(0.25)
-   if i==100:
-      print("Holding for 3 seconds")
-      sleep(3)
-      while 0<=i<=100:
-         p1.start(i)
-         p2.start(i)
-         print("Speed is at ",i,"%")
-         i-=10
-         sleep(0.25)
-   else:
-      print("It's Val's fault")
-   return render_template('home.html')
-
+   return render_template('gamepad.html')
 
 if __name__ == '__main__': 
 	app.run(host='0.0.0.0', debug=True, threaded=True)
