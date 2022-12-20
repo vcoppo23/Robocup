e286c32f3e544f5795a40af1044d7a2159bfe887
gmankowski23 <gmankowski23@bsmschool.org>
2022-12-20 12:55:04 -0600

    updates

--- app.py
+++ app.py
@@ -179,4 +179,4 @@ def video_feed():
    return Response(gen_frame(), mimetype='multipart/x-mixed-replace; boundary=frame')
    
 if __name__ == '__main__': 
-	app.run(host='0.0.0.0', debug=True, threaded=True)
+	app.run(host='0.0.0.0', debug=False, threaded=True)
