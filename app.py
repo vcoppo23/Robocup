1526b3f009cf559bc4a50ddd6f6e43faf40c768b
gmankowski23 <gmankowski23@bsmschool.org>
2023-03-02 15:17:17 -0600

    no input test

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ def valueConverter(value):
 @app.route('/mode_one', methods=['GET', 'POST'])
 def mode_one():
    #this mode controls the treads, flippers
-   if request.method == 'POST':
+   while request.method == 'POST':
 
       dictionary = json.loads(request.POST.get('values'))
 
