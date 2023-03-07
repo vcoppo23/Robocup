51392941862438f44448299ca4c9392ce9416b9f
gmankowski23 <gmankowski23@bsmschool.org>
2023-03-07 15:56:00 -0600

    QOL changes

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+from aiohttp import request
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 import json as JSON
+from colorama import Fore
     
 app = Flask(__name__)
 
@@ -12,15 +14,15 @@ def index():
 
 @socketio.on('connect')
 def test_connect():
-    print('Client connected')
-
+    print(Fore.GREEN + 'Client connected')
 @socketio.on('disconnect')
 def test_disconnect():
-    print('Client disconnected')
+    print(Fore.RED + 'Client disconnected')
 
 @socketio.on('info')
 def my_event(message):
     print(str(message))
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, host='0.0.0.0')
+    print(Fore.RED + 'Server started')
+    socketio.run(app, debug=False, host='0.0.0.0')
