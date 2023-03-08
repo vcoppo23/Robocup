99ceb19b595c9da0ba98a88516c54d51932fb0a7
gmankowski23 <gmankowski23@bsmschool.org>
2023-03-08 11:22:53 -0600

    adding controls to motors

--- app.py
+++ app.py
@@ -3,10 +3,63 @@ from flask_socketio import SocketIO, emit
 import json as JSON
 from colorama import Fore
 import os
-    
-app = Flask(__name__)
+from motorlib import *
+import RPi.GPIO as GPIO
 
+#Setup Flask and SocketIO
+app = Flask(__name__)
 socketio = SocketIO(app)
+
+#Setup GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+#Setup Motors
+RightTread = motor("io1", 1, 9)
+FrontRightFlipper = motor("io1", 5, 11)
+BackRightFlipper = motor("io1", 0, 0)
+
+LeftTread = motor("io1", 2, 10)
+FrontLeftFlipper = motor("io1", 6, 12)
+BackLeftFlipper = motor("io2", 0, 0)
+
+Turret = motor("io2", 2, 10)
+
+Shoulder = motor("io2", 1, 9)
+
+Elbow = motor("io2", 5, 11)
+
+Wrist = motor("io2", 6, 12)
+
+Claw = motor("pi", 0, 0)
+
+#Setup Power Variables
+powerP = 0.6
+powerT = 0.6
+
+def Shutdown(message):
+    if message == "true":
+        #Sets all motors to 0
+        RightTread.start(0)
+        FrontRightFlipper.start(0)
+        BackRightFlipper.start(0)
+        LeftTread.start(0)
+        FrontLeftFlipper.start(0)
+        BackLeftFlipper.start(0)
+        Turret.start(0)
+        Shoulder.start(0)
+        Elbow.start(0)
+        Wrist.start(0)
+        Claw.start(0)
+    elif message == "false":
+        pass
+
+def valueConverter(value):
+   if value == 'true':
+      return True
+   else:
+      return False  
+
 
 @app.route('/')
 def index():
@@ -15,14 +68,72 @@ def index():
 @socketio.on('connect')
 def test_connect():
     print(Fore.GREEN + 'Client connected' + Fore.RESET)
+
 @socketio.on('disconnect')
 def test_disconnect():
     print(Fore.RED + 'Client disconnected' + Fore.RESET)
 
-@socketio.on('info')
+    #Stops all motors incase of disconnect
+    shutdown = "true"
+    Shutdown(shutdown)
+
+@socketio.on('treads')
 def my_event(message):
-    os.system('clear')
-    print(str(message))
+    #os.system('clear')
+    #print(str(message))
+
+    #Checks for shutdown
+    shutdown = message['shutdown1']
+    Shutdown(shutdown)
+
+    LeftTread.start(int((float(message['joystick1'])*100)*powerP))
+    RightTread.start(int((float(message['joystick2'])*100)*powerP))
+
+    if valueConverter(message['frontLeftFlipperUp']):
+         FrontLeftFlipper.start(50)
+    elif valueConverter(message['frontLeftFlipperDown']):
+        FrontLeftFlipper.start(-50)  
+    else:
+        FrontLeftFlipper.start(0)
+
+    if valueConverter(message['frontRightFlipperUp']):
+        FrontRightFlipper.start(-50)
+    elif valueConverter(message['frontRightFlipperDown']):
+        FrontRightFlipper.start(50)
+    else:
+        FrontRightFlipper.start(0)
+    '''
+    if valueConverter(message['backLeftFlipperUp']):
+        BackLeftFlipper.start(50)
+    elif valueConverter(message['backLeftFlipperDown']):
+        BackLeftFlipper.start(-50)
+    else:
+        BackLeftFlipper.start(0)
+    if valueConverter(message['backRightFlipperUp']):
+        BackRightFlipper.start(50)
+    elif valueConverter(message['backRightFlipperDown']):
+        BackRightFlipper.start(-50)
+    else:
+        BackRightFlipper.start(0)'''
+    
+
+@socketio.on('turret')
+def my_event(message):
+    #os.system('clear')
+    #print(str(message))
+
+    #Checks for shutdown
+    shutdown = message['shutdown2']
+    Shutdown(shutdown)
+
+    Turret.start(int((float(message['turretControls'])*100)*0.3))
+    Shoulder.start(int((float(message['shoulderControls'])*100)*powerT))
+    Elbow.start(int((float(message['elbowControls'])*100)*powerT))
+    Wrist.start(int((float(message['wristControls'])*100)*powerT))
+
+    clawOpen = int(float(message['clawOpen'])*100)
+    clawClose = int(float(message['clawClose'])*100)
+    Claw.start(clawOpen - clawClose)
 
 if __name__ == '__main__':
     print(Fore.RED + 'Server started' + Fore.RESET)
