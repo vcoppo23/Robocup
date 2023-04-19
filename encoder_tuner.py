87813c658ecb642aaec482340b98ce79cd3563d7
vcoppo23 <vcoppo23@bsmschool.org>
2023-04-19 14:27:04 -0500

    waad

--- encoder_tuner.py
+++ encoder_tuner.py
@@ -1,2 +1,27 @@
-def tune_encoder(gear_ratio,slices):
-    return 
+import keyboard 
+import motorlib
+import time
+slices = 10
+speedlist = []
+def tune_encoder(motor):
+    if motor.encoder == None:
+        print ("This motor does not have an encoder attatched")
+        return
+
+    for i in range(0,slices):
+
+        while keyboard.is_pressed('q') == False:
+            motor.start(i*10)
+            angle = motor.get_angle()
+            print (angle)
+        else:
+            motor.stop()
+            speedlist.append(angle)
+            time.sleep(2)
+
+    print (speedlist)
+
+
+
+
+        
