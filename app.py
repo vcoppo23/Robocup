5d823e1a5de1c4b287f7ac14925251a0f3a97b78
gmankowski23 <gmankowski23@bsmschool.org>
2023-06-21 12:52:15 -0500

    comments to run

--- app.py
+++ app.py
@@ -1,7 +1,3 @@
-# Robocup'23 - Motors
-# BSM Robotics
-# Val Coppo and Gabriel Mankowski
-
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit ## SocketIO is a python library that allows socket use for flask servers
 from colorama import Fore ## Just so messages are different colors
@@ -17,27 +13,27 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 #Setup Boards
-io1 = board(0x18)
-io2 = board(0x19)
+#io1 = board(0x18)
+#io2 = board(0x19)
 
 #Setup Motors
-RightTread = motor(io1,pins = [1, 11]) ## In the order of (board, PWM pin, DIR pin)
-FrontRightFlipper = motor(io1, pins = [3, 9])
-BackRightFlipper = motor(io1, pins = [5, 7])
+RightTread = motor('pi',pins = [1, 11]) ## In the order of (board, PWM pin, DIR pin)
+#FrontRightFlipper = motor(io1, pins = [3, 9])
+#BackRightFlipper = motor(io1, pins = [5, 7])
 
-LeftTread = motor(io1, pins = [2, 12])
-FrontLeftFlipper = motor(io1, pins = [4, 10])
-BackLeftFlipper = motor(io1, pins = [6, 8])
+LeftTread = motor('pi', pins = [2, 12])
+#FrontLeftFlipper = motor(io1, pins = [4, 10])
+#BackLeftFlipper = motor(io1, pins = [6, 8])
 
-Turret = motor(io2, pins = [2, 10])
+#Turret = motor(io2, pins = [2, 10])
 
-Shoulder = motor(io2, pins = [1, 9])
+#Shoulder = motor(io2, pins = [1, 9])
 
-Elbow = motor(io2, pins = [5, 11])
+#Elbow = motor(io2, pins = [5, 11])
 
-Wrist = motor(io2, pins =[ 6, 12])
+#Wrist = motor(io2, pins =[ 6, 12])
 
-Claw = motor('pi', pins = [18, 23])
+#Claw = motor('pi', pins = [18, 23])
 
 #Setup Power Variables
 # This can be changed to scale the power of the motors for the specific subsystems
@@ -48,16 +44,16 @@ def Shutdown(message):
     if message: ## check later for correct functionality
         #Sets all motors to 0
         RightTread.start(0)
-        FrontRightFlipper.start(0)
-        BackRightFlipper.start(0)
+        #FrontRightFlipper.start(0)
+        #BackRightFlipper.start(0)
         LeftTread.start(0)
-        FrontLeftFlipper.start(0)
-        BackLeftFlipper.start(0)
-        Turret.start(0)
-        Shoulder.start(0)
-        Elbow.start(0)
-        Wrist.start(0)
-        Claw.start(0)
+        #FrontLeftFlipper.start(0)
+        #BackLeftFlipper.start(0)
+        #Turret.start(0)
+        #Shoulder.start(0)
+        #Elbow.start(0)
+        #Wrist.start(0)
+        #Claw.start(0)
         
         print(Fore.RED + 'Shutdown' + Fore.RESET)
 
@@ -88,7 +84,7 @@ def my_event(message):
 
     LeftTread.start(int((float(message['joystick1'])*100)*powerDrive))
     RightTread.start(int((float(message['joystick2'])*100)*powerDrive))
-
+    '''
     if (message['frontLeftFlipperUp']):
          print (message['frontLeftFlipperUp'])
          FrontLeftFlipper.start(50)
@@ -116,10 +112,11 @@ def my_event(message):
         BackRightFlipper.start(-50)
     else:
         BackRightFlipper.start(0)
+    '''
     
     print("LeftPower: " + str(int((float(message['joystick1'])*100)*powerDrive)) + " RightPower: " + str(int((float(message['joystick2'])*100)*powerDrive)))
     
-
+'''
 @socketio.on('turret')
 def my_event(message):
     #os.system('clear')
@@ -136,7 +133,7 @@ def my_event(message):
 
     clawOpen = int(float(message['clawOpen'])*100)
     clawClose = int(float(message['clawClose'])*100)
-
+'''
 if __name__ == '__main__':
     print(Fore.RED + 'Server started' + Fore.RESET)
     socketio.run(app, debug=False, host='0.0.0.0')
